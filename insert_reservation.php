bf07468dcdc0b0c0a0b5bd548c772aec292cb1a2
Amel Chabah <98820981+amelchabah@users.noreply.github.com>
2022-05-15 16:46:23 +0200

    Update insert_reservation.php

--- insert_reservation.php
+++ insert_reservation.php
@@ -1,41 +1,22 @@
 <?php
-
 /*inserer les infos privees dans la bdd + mail aux client ET gestionnaire*/
 
-
-
 include("connexion.php");
-
 $nom_destination = $_POST["nom_destination"];
-
 $nb_voyageurs = $_POST["nb_voyageurs"];
-
 $date_depart = $_POST["date_depart"];
-
 $date_retour = $_POST["date_retour"];
-
 $lieu_depart = $_POST["lieu_depart"];
-
 $client_nom = $_POST["client_nom"];
-
 $client_prenom = $_POST["client_prenom"];
-
 $client_mail = $_POST["client_mail"];
-
 $client_tel = $_POST["client_tel"];
-
 $prix = $_POST["prix"];
-
 $prix_total = intval($nb_voyageurs) * intval($prix);
 
-
-
 /*inserer ces donnees dans la bdd...*/
-
 $requete = "INSERT INTO reservation (nom_destination,nb_voyageurs,date_depart,date_retour,lieu_depart,client_nom,client_prenom,client_mail,client_tel,prix_total) VALUES ('$nom_destination','$nb_voyageurs','$date_depart','$date_retour','$lieu_depart','$client_nom','$client_prenom','$client_mail','$client_tel','$prix_total')";
-
 $db->query($requete);
-
 
 
 
@@ -67,30 +48,16 @@ mail($mailTo, $subject, $message, $headers);
 header("Location: thankyou.html");
 ?>
 
-
-
 <?php
-
 /*ecriture du mail pour le gestionnaire, en html*/
 
-
-
 $headers = "Content-type: text/html; charset=utf-8\n";
-
 $subject = "New booking!";
-
 $mailTo = "amel.chabah@edu.univ-eiffel.fr";
-
 $message = '<div style="font-family:noto sans, sans-serif"><h4>' . $client_prenom . ' ' . $client_nom . ' booked a trip for <b>' . $nom_destination . '</b> :</h4><p>From <b>' . $lieu_depart . '</b>, ' . $date_depart . ' to ' . $date_retour . ' for ' . $nb_voyageurs . ' passenger(s).</p>
-
 <ul><li><b>Total price</b> : $ ' . number_format($prix_total, 0, ".", ",") . '</li><li><b>Mail</b> : ' . $client_mail . '</li><li><b>Phone number</b> : ' . $client_tel . '</li></ul></div>';
 
-
-
 mail($mailTo, $subject, $message, $headers);
-
 header("Location: thankyou.html");
-
-
 
 ?>
