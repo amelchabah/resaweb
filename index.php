b036050cd99224c2500f2c881c93a82b240e40b6
Amel Chabah <98820981+amelchabah@users.noreply.github.com>
2022-05-17 11:47:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="Landing page of Galactrip, a fictional space travel agency. Here you'll find a short description of our concept and our latest destinations.">
 
 
 
